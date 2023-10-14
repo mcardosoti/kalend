b5cadb458dbda9e3ec007fae9b4642a932b427b5
mcardosoti <mcardosoti@gmail.com>
2023-10-14 00:06:15 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Array para armazenar os eventos
+const events = [];
+
+// Função para adicionar um evento
+function addEvent() {
+  const title = document.getElementById("event-title").value;
+  const date = document.getElementById("event-date").value;
+
+  if (title && date) {
+    events.push({ title, date });
+    updateEventList();
+    clearEventForm();
+  }
+}
+
+// Função para listar os eventos
+function updateEventList() {
+  const eventsList = document.getElementById("events");
+  eventsList.innerHTML = "";
+
+  for (const event of events) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `${event.title} - ${event.date}`;
+    eventsList.appendChild(listItem);
+  }
+}
+
+// Função para limpar o formulário
+function clearEventForm() {
+  document.getElementById("event-title").value = "";
+  document.getElementById("event-date").value = "";
+}
+
+// Pode ser expandido com recursos de armazenamento persistente (por exemplo, local storage) e interações mais avançadas.
